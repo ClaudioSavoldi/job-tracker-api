799857a58a1b91499ea291fd45789ddd4e9b5395
Claudio Savoldi <savoldiclaudio02@gmail.com>
2026-03-20 17:29:16 +0100

    Add authentication register flow with Identity validation

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,53 @@
+﻿using JobTracker.API.Dtos.Requests;
+using JobTracker.API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AuthController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequestDto registerDto)
+        {
+            //controllo se il dto ricevuto rispetta le regole di validazione               
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            //controllo se la mail con cui ci si registra esiste gia
+            var existingUser = await _userManager.FindByEmailAsync(registerDto.Email);
+            if(existingUser != null)
+            {
+                return BadRequest("Email already exists!");
+            }
+            //creo l`utente
+            var user = new ApplicationUser
+            {
+                FirstName = registerDto.FirstName,
+                LastName = registerDto.LastName,
+                Email = registerDto.Email,
+                UserName = registerDto.Email
+            };
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+            //controllo se l`utente è stato creato correttamente
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+            //restituire successo
+            return StatusCode(201, "User registered successfully!");
+
+        }
+    }
+}
---
+++ Dtos/Requests/RegisterRequestDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobTracker.API.Dtos.Requests
+{
+    public class RegisterRequestDto
+    {
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; } = string.Empty;
+
+    }
+}
